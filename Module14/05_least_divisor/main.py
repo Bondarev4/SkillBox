ee00122a3bf112de815c68b1e5798007d66e3548
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-11-29 21:32:29 +0500

    Add lesson

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def min_devider(x):
+    dev = 0
+    for number in range(2, x + 1):
+        if x % number == 0:
+            dev = number
+            break
+    return dev
+
+x = int(input('Введите число: '))
+print('Наименьший делитель, отличный от единицы:', min_devider(x))
