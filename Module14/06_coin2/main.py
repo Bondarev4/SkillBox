ed6097bed2fd34c6f3d6605a7eb7d01ec0329108
Bondarev Ivan <bledmeil001nk@mail.ru>
2021-11-29 21:37:57 +0500

    Add lesson

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def around(x, y, r):
+    if x ** 2 + y ** 2 <= r:
+        return True
+    return False
+
+x = float(input('Введите координаты монетки:\nX: '))
+y = float(input('Y: '))
+r = int(input('Введите радиус: '))
+if around(x, y, r):
+    print('Монетка где-то рядом')
+else:
+    print('Монетки в области нет')
--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+def simil_number(x, number):
+    k = 0
+    while x > 0:
+        if x % 10 == number:
+            k += 1
+        x //= 10
+    return k == 3
+
+def simil_num_3(x):
+    while x > 0:
+        d = x % 10
+        if simil_number(x, d):
+            return True
+        x //= 10
+    return False
+
+start = int(input('Введите первый год: '))
+stop = int(input('Введите второй год: '))
+print('Года от', start, 'до', stop, 'с тремя одинаковыми цифрами:')
+for year in range(start, stop + 1):
+    if simil_num_3(year):
+        print(year)
