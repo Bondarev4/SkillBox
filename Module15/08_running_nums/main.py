132d925369825f4499254233c185fba5f14965b3
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-11-30 20:07:39 +0500

    Add lesson

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+list = [int(x) for x in input('Изначальный список: ').split()]
+k = int(input('Сдвиг: '))
+n = len(list)
+k %= n
+list1 = [0] * n
+for i in range(n):
+    list1[(i + k) % n] = list[i]
+print('Сдвинутый список: ', list1)
--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+a = [int(x) for x in input('Введите список: ').split()]
+j = 1
+n = len(a)
+while j < n:
+   for i in range(n - j):
+       if a[i] > a[i + 1]:
+           a[i], a[i + 1] = a[i + 1], a[i]
+   j += 1
+print('Отсортированный список:', *a)
