081966036442fd631db90d219fb04e3ebe27c253
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-02 18:05:42 +0500

    3 task

--- Module16/03_details/main.py
+++ Module16/03_details/main.py
@@ -2,4 +2,12 @@ shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300],
         ['педаль', 100], ['седло', 1500], ['рама', 12000],
         ['обод', 2000], ['шатун', 200], ['седло', 2700]]
 
-# TODO здесь писать код
+detail = input('Название детали: ')
+quantity = 0
+sum_prize = 0
+for i in range(len(shop)):
+        if shop[i][0] == detail:
+                quantity += 1
+                sum_prize += shop[i][1]
+print('\nКол-во деталей -', quantity)
+print('Общая стоимость -', sum_prize)
