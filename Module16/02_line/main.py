ebd6f7523acab205529fb9e3a236d528b64be57d
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-02 17:38:53 +0500

    2 task

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def sort(a):
+    j = 1
+    n = len(a)
+    while j < n:
+        for i in range(n - j):
+            if a[i] > a[i + 1]:
+                a[i], a[i + 1] = a[i + 1], a[i]
+        j += 1
+
+first_class = list(range(160, 177, 2))
+second_class = list(range(162, 181, 3))
+first_class.extend(second_class)
+sort(first_class)
+print(first_class)
+
