e6f364904ac69780e2898b790b975d6aba75ace0
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-03 23:29:39 +0500

    task 10

--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+def pal(ar):
+    for i in range(len(ar) // 2):
+        if ar[i] != ar[-1 - i]:
+            return False
+    return True
+def check(a):
+    a1 = a
+    counter = 0
+    arr = []
+    while not pal(a1):
+        a1 = []
+        counter += 1
+        arr.insert(0, a[counter - 1])
+        a1.extend(a)
+        a1.extend(arr)
+    return counter, arr
+
+
+n = int(input('Кол-во чисел: '))
+a = [int(x) for x in input('Введите последовательность чисел: ').split()]
+print('\nПоследовательность:', *a)
+count, array = check(a)
+print('Нужно приписать чисел:', count)
+print('Сами числа:', * array)
