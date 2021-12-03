bc0835d6b8662cc95a6956b8ffc18d6ba0ee2633
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-03 22:39:34 +0500

    task 9

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+n = int(input('Кол-во друзей: '))
+k = int(input('Долговых расписок: '))
+friends = [0] * n
+for i in range(k):
+    print()
+    print(i + 1, 'расписка')
+    to = int(input('Кому: '))
+    who = int(input('От кого: '))
+    money = int(input('Сколько: '))
+    friends[to - 1] -= money
+    friends[who - 1] += money
+print('\nБаланс друзей:')
+for i in range(n):
+    print(i + 1, ':', friends[i])
