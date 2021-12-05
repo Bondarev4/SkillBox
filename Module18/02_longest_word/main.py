208fc0403a3f66b069ea7b46e21ff005fd6e26b8
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-05 20:40:32 +0500

    homework

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+string = input('Введите строку: ').split()
+max_long = len(string[0])
+this_word = string[0]
+for word in string:
+    n = len(word)
+    if n > max_long:
+        max_long = n
+        this_word = word
+print(this_word)
