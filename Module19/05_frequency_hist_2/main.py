3cbce6f0287b754a9c9b32aab307d0e98247b46c
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-13 19:52:36 +0500

    homework

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+string = input('Введите текст: ')
+counter_alpha = {}
+been = []
+for letter in string:
+    if letter not in been:
+        k = string.count(letter)
+        if k not in counter_alpha:
+            counter_alpha[k] = [letter]
+        else:
+            counter_alpha[k].append(letter)
+        been.append(letter)
+print('Инвертированный словарь частот:')
+for key in counter_alpha.keys():
+    print(key, ':', counter_alpha[key])
--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+n = int(input('Введите количество пар слов: '))
+synonyms = {}
+for i in range(1, n + 1):
+    pair = input(str(i) + ' пара: ').lower().split(' - ')
+    synonyms[pair[0]] = pair[1]
+    synonyms[pair[1]] = pair[0]
+print()
+while True:
+    word = input('Введите слово: ').lower()
+    if word in synonyms:
+        print('Синоним:', synonyms[word].capitalize())
+    else:
+        print('Такого слова в словаре нет.')
