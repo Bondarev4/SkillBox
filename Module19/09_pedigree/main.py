984d78429b52286756a654aaee12765c95164c80
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-14 17:40:11 +0500

    homework

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+number = int(input('Введите количество человек: '))
+members = dict()
+first_p, second_p = input('1 пара: ').split()
+members[second_p] = 0
+members[first_p] = members[second_p] + 1
+for i in range(2, number):
+    first_p, second_p = input(str(i) + ' пара: ').split()
+    members[first_p] = members[second_p] + 1
+print('\n“Высота” каждого члена семьи:')
+for name in sorted(members):
+    print(name, members[name])
--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+string = input('Введите строку: ')
+letters = set(string)
+flag = True
+if len(string) % 2 == 0:
+    for el in string:
+        if string.count(el) % 2 != 0:
+            flag = False
+            break
+else:
+    flag_odd = False
+    for el in string:
+        if string.count(el) % 2 == 1:
+            if flag_odd:
+                flag = False
+                break
+            flag_odd = True
+if flag:
+    print('Можно сделать палиндромом')
+else:
+    print('Нельзя сделать палиндромом')
