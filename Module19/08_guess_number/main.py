125319f0e5bcdfbf16d74a7e9c6774aec7f19bf4
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-14 16:38:17 +0500

    homework

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+max_n = int(input('Введите максимальное число: '))
+numbers = set(range(1, max_n + 1))
+while True:
+    ans_b = input('\nНужное число есть среди вот этих чисел: ')
+    if ans_b == 'Помогите!':
+        ans_a = set(int(x) for x in input('Артём мог загадать следующие числа: ').split())
+        numbers = ans_a & numbers
+    else:
+        ans_b = set(map(int, ans_b.split()))
+        ans_a = input('Ответ Артёма: ')
+        if ans_a == 'Да':
+            if len(ans_b) == 1:
+                print('\nБорис угадал число. Ура!')
+                break
+            else:
+                numbers = numbers & ans_b
+        else:
+            numbers = numbers.difference(ans_b)
