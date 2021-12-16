4a5da1812dce8bd043b3dd750e01120184c2605e
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-16 20:46:04 +0500

    homework

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+n = int(input('Сколько записей вносится в протокол? '))
+records = {}
+print('Записи (результат и имя):')
+for i in range(1, n + 1):
+    score, name = input(' '.join([str(i), 'запись: '])).split()
+    if score in records:
+        records[int(score)] += name
+    else:
+        records[int(score)] = [name]
+print('\nИтоги соревнований:')
+place = 1
+for score in sorted(records):
+    k = len(records[score])
+    if k > 1:
+        for el in records[score]:
+            print('{} место.'.format(place), score, *records[score])
+            place += 1
+    else:
+        print('{} место.'.format(place), score, *records[score])
+        place += 1
