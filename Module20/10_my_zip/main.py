52c9aac802f2eb32ad2d1ec39a34eb37bb01e0cf
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-17 17:13:27 +0500

    homework

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,4 +1,7 @@
 string = input('Строка: ')
 trp = (int(x) for x in input('Кортеж чисел: ').split())
-analogue_zip = []
-for x, y in
+s_l = len(string)
+analogue_zip = ((string[i], el) for i, el in enumerate(trp) if i < s_l )
+print('\nРезультат:', analogue_zip)
+for x in analogue_zip:
+    print(x)
