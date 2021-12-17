f632cc54da7b8aa93c31d79a12e79391ed5e1c00
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-17 07:54:28 +0500

    homework

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -3,18 +3,13 @@ records = {}
 print('Записи (результат и имя):')
 for i in range(1, n + 1):
     score, name = input(' '.join([str(i), 'запись: '])).split()
-    if score in records:
-        records[int(score)] += name
+    if int(score) in records:
+        records[int(score)].append(name)
     else:
         records[int(score)] = [name]
 print('\nИтоги соревнований:')
 place = 1
-for score in sorted(records):
-    k = len(records[score])
-    if k > 1:
-        for el in records[score]:
-            print('{} место.'.format(place), score, *records[score])
-            place += 1
-    else:
-        print('{} место.'.format(place), score, *records[score])
+for score in sorted(records)[::-1]:
+    for nick in records[score]:
+        print('{} место. {} ({})'.format(place, nick, score))
         place += 1
