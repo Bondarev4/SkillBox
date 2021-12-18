7e7c8bdac4171c54551729318ecc1b0a3426af07
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-18 12:39:44 +0500

    homework

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def series_of_number(x, number):
+    if x > number:
+        return x
+    print(x)
+    return series_of_number(x + 1, number)
+
+
+n = int(input('Введите num: '))
+series_of_number(1, n)
