ab7d9aa74535bd247799ba075174e47d566e0752
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-19 01:05:06 +0500

    homework

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def f_num(pos, f=1, s=1):
+    if pos == 1:
+        return f + s
+    return f_num(pos - 1, s, f + s)
+
+
+num_pos = int(input('Введите позицию числа в ряде Фибоначчи: '))
+if num_pos < 3:
+    print(1)
+else:
+    print('\nЧисло: {}'.format(f_num(num_pos - 2)))
--- Module21/04_search_element_2/main.py
+++ Module21/04_search_element_2/main.py
@@ -1,1 +1,31 @@
-# TODO здесь писать код
+def found_key(struct, k, *args):
+    # if args == 0:
+    #     return None
+    # if k in struct:
+    #     return k, struct[k]
+    args -= 1
+    for key in struct.values():
+        result = found_key(struct[key], k, *args)
+        if result != 'None'
+    return print('Значение ключа:', struct.get(key))
+
+site = {
+    'html': {
+        'head': {
+            'title': 'Мой сайт'
+        },
+        'body': {
+            'h2': 'Здесь будет мой заголовок',
+            'div': 'Тут, наверное, какой-то блок',
+            'p': 'А вот здесь новый абзац'
+        }
+    }
+}
+
+key = input('Введите искомый ключ: ')
+flag_d = input('Хотите ввести максимальную глубину? Y/N: ').lower()
+if flag_d == 'y':
+    m_depth = int(input('Введите максимальную глубину: '))
+    found_k = found_key(site, key, m_depth)
+else:
+    found_k = found_key(site, key)
