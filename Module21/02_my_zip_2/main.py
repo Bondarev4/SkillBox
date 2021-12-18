8efa1115789227677be10c5697edbcc4f4ea0869
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-18 17:46:10 +0500

    homework

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def my_zip(*args):
+    return [tuple(arr[i] for arr in args) for i in range(min(map(len, args)))]
+
+
+# string = input('Строка: ')
+# tp = (int(x) for x in input('Кортеж чисел: ').split())
+# lst_tp = list(tp)
+# new_tpl = my_zip(string, lst_tp)
+# print(new_tpl)
