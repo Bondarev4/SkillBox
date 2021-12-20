b12405826095862ea984722ec6bcd84492376c68
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-20 21:28:08 +0500

    homework

--- Module21/04_search_element_2/main.py
+++ Module21/04_search_element_2/main.py
@@ -1,13 +1,15 @@
-def found_key(struct, k, *args):
-    # if args == 0:
-    #     return None
-    # if k in struct:
-    #     return k, struct[k]
-    args -= 1
-    for key in struct.values():
-        result = found_key(struct[key], k, *args)
-        if result != 'None'
-    return print('Значение ключа:', struct.get(key))
+def find_key(struct, k, depth):
+    if k in struct:
+        return struct[k]
+    if depth > 1:
+        for value in struct.values():
+            if isinstance(value, dict):
+                result = find_key(value, k, depth - 1)
+                if result:
+                    break
+        else:
+            result = None
+        return result
 
 site = {
     'html': {
@@ -26,6 +28,7 @@ key = input('Введите искомый ключ: ')
 flag_d = input('Хотите ввести максимальную глубину? Y/N: ').lower()
 if flag_d == 'y':
     m_depth = int(input('Введите максимальную глубину: '))
-    found_k = found_key(site, key, m_depth)
-else:
-    found_k = found_key(site, key)
+    found_k = find_key(site, key, m_depth)
+elif flag_d == 'n':
+    found_k = find_key(site, key, )
+print('Значение ключа: {}'.format(found_k))
