4deefba23e735663d1992792a882cbf73a7ed14b
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-21 20:03:19 +0500

    homework

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -1,3 +1,16 @@
+def site_copy(struct, product, new_site):
+    if 'title' == struct:
+        new_site['title'] = 'Куплю/продам {} недорого'.format(product)
+    elif 'h2' == struct:
+        new_site['h2'] = 'У нас самая низкая цена на {}'.format(product)
+    else:
+        new_site[struct] = site[struct]
+    for value in struct.values():
+        if isinstance(value, dict):
+            return site_copy(value, product, new_site)
+    return new_site
+
+
 site = {
     'html': {
         'head': {
@@ -10,5 +23,8 @@ site = {
         }
     }
 }
-
-# TODO здесь писать код
+n = int(input('Сколько сайтов: '))
+n_site = {}
+for i in range(n):
+    name_of_product = input('Введите название продукта для нового сайта: ')
+    print(site, name_of_product, n_site)
