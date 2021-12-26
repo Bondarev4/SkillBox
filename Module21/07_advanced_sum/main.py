655fce790eb96ce81675148340a18409c1f37237
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-26 15:40:39 +0500

    homework

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,12 +1,4 @@
-def mega_sum(*args):
-    s = 0
-    for element, i in enumerate(args):
-        if isinstance(element, int):
-            s += element
-        else:
-            s += mega_sum(element[i])
-    return s
 
 
-print(mega_sum([[1, 2, [3]], [1], 3]))
-# print(mega_sum(1, 2, 3, 4, 5))
+
+
--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,14 @@
+def reveal_lst(lst, n_lst):
+    for el in lst:
+        if isinstance(el, list):
+            reveal_lst(el, n_lst)
+        else:
+            n_lst.append(el)
+
+
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+new_lst = list()
+reveal_lst(nice_list, new_lst)
+print(new_lst)
