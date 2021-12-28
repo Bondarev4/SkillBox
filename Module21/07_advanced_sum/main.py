da094ef5c499dd660e9469b61929f99e18a682b1
Bondarev Ivan <bledMeil001nk@mail.ru>
2021-12-28 11:39:55 +0500

    homework

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,4 +1,13 @@
+def sum_p(*args):
+    s = 0
+    for element in args:
+        if isinstance(element, int):
+            s += element
+        else:
+            for i, v in enumerate(element):
+                s += sum_p(v)
+    return s
 
 
-
-
+# print(sum_p([[1, 2, [3]], [1], 3]))
+# print((sum_p(1, 2, 3, 4, 5)))
