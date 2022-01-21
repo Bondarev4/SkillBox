47a4d0c5a1228ca95b3e5e63f8bd6b6ab21399ac
Bondarev Ivan <bledMeil001nk@mail.ru>
2022-01-21 16:20:10 +0500

    homework

--- Module22/04_files_and_folders/main.py
+++ Module22/04_files_and_folders/main.py
@@ -2,20 +2,23 @@ import os
 
 
 def calculate(directory, size=0, catalogs=0, files=0):
-    print(directory)
+    print(directory, size, catalogs, files)
     for el in os.listdir(directory):
         path_el = os.path.join(directory, el)
         if os.path.isdir(path_el):
             catalogs += 1
-            calculate(path_el, size, catalogs, files)
+            s, c, f = calculate(path_el)
+            files += f
+            catalogs += c
+            size += s
         else:
             size += os.path.getsize(path_el)
             files += 1
     return size, catalogs, files
-
+# C:\SkillBox\pythonProject\Python_Basic\Module14
 
 path = input('Пожалуйста, введите путь до директории: ')
 s_size, s_catalogs, s_files = calculate(path)
-print('Размер каталога (в Кб): {}\nКоличество подкаталогов: {}\nКоличество файлов: {}'.format(s_size,
+print('Размер каталога (в Кб): {}\nКоличество подкаталогов: {}\nКоличество файлов: {}'.format(s_size / 1024,
                                                                                               s_catalogs,
                                                                                               s_files))
