e8db4ddbf172924de0ad40c204fbd9115ea7929d
Bondarev Ivan <bledMeil001nk@mail.ru>
2022-01-21 21:13:24 +0500

    homework

--- Module22/06_paranoia/main.py
+++ Module22/06_paranoia/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+import os
+
+l_l = 'abcdefghijklmnopqrstuvwxyz'
+h_l = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def caeser_text(text):
+
+
+
+file = open('text.txt', 'r')
+print('Содержимое файла text.txt:')
+for line in file:
+    print(line)
+caeser_text(file.read())
+print('Содержимое файла cipher_text.txt:')
+file.close()
