c7101c66817e57e214781c3b48d88c20cdf90b30
Bondarev Ivan <bledMeil001nk@mail.ru>
2022-01-22 16:52:11 +0500

    homework

--- Module22/08_frequency_analysis/main.py
+++ Module22/08_frequency_analysis/main.py
@@ -1,1 +1,29 @@
-# TODO здесь писать код
+def analysis_text(text):
+    t_a = open('analysis.txt', 'w')
+    letters = {}
+    for letter in text.read().lower():
+        if letter.isalpha():
+            if letter in letters:
+                letters[letter] += 1
+            else:
+                letters[letter] = 1
+    sort_letters = {el: letters[el] for el in sorted(letters, key=letters.get, reverse=True)}
+    sum_letter = sum(sort_letters.values())
+    for sym, count in sort_letters.items():
+        t_a.write(sym + ' ' + str(round((count / sum_letter), 3)) + '\n')
+    t_a.close()
+
+
+file_text = open('text.txt', 'r')
+print('Содержимое файла text.txt:')
+print(file_text.read())
+
+file_text = open('text.txt', 'r')
+analysis_text(file_text)
+
+file_analysis = open('analysis.txt', 'r')
+print('\nСодержимое файла analysis.txt:')
+print(file_analysis.read())
+
+file_text.close()
+file_analysis.close()
