e28c1e1ca610f4d58c0932b2a270e3b437de1610
Bondarev Ivan <bledMeil001nk@mail.ru>
2022-01-25 20:46:26 +0500

    homework

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -1,25 +1,32 @@
 import random
 
 
-def random_delete(text):
-    unlucky_line = random.randint(0, text.read().count('\n'))
-    lines_list = text.readlines()
-    lines_list[unlucky_line] = '\n'
-    text.writelines(lines_list)
-
-
 s = 0
-with open('lucky_number.txt', 'w') as file:
-    while s < 777:
-        try:
-            if random.randint(1, 13) == 13:
-                random_delete(file)
+try:
+    with open('lucky_number.txt', 'w+') as file:
+        while s < 777:
+            if random.randint(1, 6) == 4:
                 raise TypeError
             number = int(input('Введите число: '))
             s += number
             file.write(str(number) + '\n')
-        except TypeError:
-            print('Вас постигла неудача!')
-with open('lucky_number.txt', 'r') as file_2:
-    print('\nСодержимое файла out_file.txt:')
-    print(file_2.read())
+
+except TypeError:
+    print('Вас постигла неудача!')
+    with open('lucky_number.txt', 'r+') as file_1:
+        print('----------')
+        count_lines = file_1.read().count('\n')
+        if count_lines != 0:
+            unlucky_line = random.randint(0, count_lines - 1)
+
+            lines_list = file_1.readlines()
+            print(lines_list)
+            # lines_list[] = '\n'
+            print(lines_list)
+            print('----------')
+            # file.writelines(lines_list)
+
+finally:
+    with open('lucky_number.txt', 'r') as file_2:
+        print('\nСодержимое файла out_file.txt:')
+        print(file_2.read())
