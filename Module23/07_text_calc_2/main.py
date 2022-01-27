0192a13b57735ae5739e9c333ee04b3ab614e6de
Bondarev Ivan <bledMeil001nk@mail.ru>
2022-01-27 20:42:51 +0500

    homework

--- Module23/07_text_calc_2/main.py
+++ Module23/07_text_calc_2/main.py
@@ -5,10 +5,21 @@ with open('calc.txt', 'r') as file:
         signs = ['+', '-', '*', '/', '%', '^']
         sign = data[1]
         try:
-            if len(data) != 3 or not data[0].isdigit() or not data[2].isdigit() or not sign in signs:
+            if len(data) != 3 or not data[0].isdigit() or not data[2].isdigit() or sign not in signs:
                 raise ValueError
         except ValueError:
-            fix = input('Обнаружена ошибка в строке: {}\tХотите исправить? '.format(line))
+            fix_flag = input('Обнаружена ошибка в строке: {}\tХотите исправить? '.format(line)).lower()
+            if fix_flag == 'нет':
+                continue
+            else:
+                fix = input('Введите исправленную строку: ')
+                with open('calc.txt', 'r') as file_a:
+                    lines_list = file_a.readlines()
+                    lines_list[lines_list.index(line)] = fix + '\n'
+                with open('calc.txt', 'w') as file_b:
+                    file_b.writelines(lines_list)
+                data = fix.split()
+                sign = data[1]
         a, b = int(data[0]), int(data[2])
         if sign == '+':
             s += a + b
