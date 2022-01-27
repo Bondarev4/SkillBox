d025b0d5c1a038f161f5dae28a00c9386134eaa3
Bondarev Ivan <bledMeil001nk@mail.ru>
2022-01-27 19:00:21 +0500

    homework

--- Module23/06_chat/main.py
+++ Module23/06_chat/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+while True:
+    action = input('\nФункции: Посмотреть текущий текст чата - 1 | Отправить сообщение  - 2\n')
+    if action == '1':
+        with open('chat.txt', 'r', encoding='utf-8') as chat_r:
+            print('\n', chat_r.read())
+    elif action == '2':
+        message = input('\nВведите сообщение:\n')
+        with open('chat.txt', 'a', encoding='utf-8') as chat_w:
+            chat_w.write(message + '\n')
+    else:
+        print('Ошибка ввода!')
--- Module23/07_text_calc_2/main.py
+++ Module23/07_text_calc_2/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+s = 0
+with open('calc.txt', 'r') as file:
+    for line in file:
+        data = line.split()
+        signs = ['+', '-', '*', '/', '%', '^']
+        sign = data[1]
+        try:
+            if len(data) != 3 or not data[0].isdigit() or not data[2].isdigit() or not sign in signs:
+                raise ValueError
+        except ValueError:
+            fix = input('Обнаружена ошибка в строке: {}\tХотите исправить? '.format(line))
+        a, b = int(data[0]), int(data[2])
+        if sign == '+':
+            s += a + b
+        elif sign == '-':
+            s += a - b
+        elif sign == '*':
+            s += a * b
+        elif sign == '/':
+            s += a // b
+        elif sign == '%':
+            s += a % b
+        else:
+            s += a ^ b
+print(s)
