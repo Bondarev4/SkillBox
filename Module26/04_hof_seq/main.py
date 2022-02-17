0590f9a8cca8f8f373e0c2071552266cae8c40cc
Bondarev Ivan <bledMeil001nk@mail.ru>
2022-02-17 18:25:04 +0500

    homework

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+class QHofstadter(object):
+    def __init__(self, lst):
+        self.lst = lst[:]
+
+    def __next__(self):
+        s = self.lst[-self.lst[-1]] + self.lst[-self.lst[-2]]
+        self.lst.append(s)
+        return s
+
+    def __iter__(self):
+        return self
+
+
+a, b = map(int, input('Введите 2 числа: ').split())
+if a == 1 and b == 2:
+    print('Немедленная остановка!')
+    exit()
+elif a < 0 and b < 0:
+    print('Ошибка ввода!')
+    exit()
+q = QHofstadter([a, b])
+row = [next(q) for _ in range(25)]
+print(*row)
--- Module26/05_str_count/main.py
+++ Module26/05_str_count/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+import os
+from pathlib import Path
+
+
+def strings_in(directory):
+    for file in os.listdir(directory):
+        counter = 0
+        if Path(file).suffixes[0] == '.py':
+            with open(file, 'r') as str_file:
+                for string in str_file:
+                    string = string.split()
+                    if len(string) != 0 and string[0] != '#':
+                        counter += 1
+                    # print(string, counter, sep='------')  # check
+            yield counter
+    return
+
+
+#
+#
+my_directory = os.path.abspath('')
+for count in strings_in(my_directory):
+    print(count)
